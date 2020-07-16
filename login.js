39af757a3e450930d60245c27f8ee23f35dc842f
creator1871 <65519405+creator1871@users.noreply.github.com>
2020-07-16 08:00:29 +0700

    Update login.js

--- login.js
+++ login.js
@@ -4,12 +4,12 @@ function validate(){
 var password = document.getElementById("password").value;
 if ( password == "cool"){
 alert ("Login successfully");
-window.location = "success.html"; // Redirecting to other page.
+window.location = "suc.html"; // Redirecting to other page.
 return false;
 }
 else{
 attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
+alert("password e salah jon"+attempt+" attempt;");
 // Disabling fields after 3 attempts.
 if( attempt == 0){
 document.getElementById("password").disabled = true;
